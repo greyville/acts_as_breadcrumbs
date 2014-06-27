60318120694a43130b09f241a919b6383efc5785
Oliver Beddows <oliver.beddows@googlemail.com>
2014-06-27 13:47:27 +0100

    Upgrading Rakefile and spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 begin
   require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
 rescue LoadError
-  puts "You need to install rspec in your base app"
+  puts "You need to install RSpec in your base app"
   exit
 end
 
@@ -9,9 +9,11 @@ plugin_spec_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 
 databases = YAML::load(IO.read(plugin_spec_dir + "/db/database.yml"))
-ActiveRecord::Base.establish_connection(databases[ENV["DB"] || "sqlite3"])
+databases.symbolize_keys!
+
+ActiveRecord::Base.establish_connection(:adapter => databases[:sqlite3][:adapter], :database => databases[:sqlite3][:database])
 load(File.join(plugin_spec_dir, "db", "schema.rb"))
 
-Spec::Runner.configure do |config|
-  config.fixture_path = plugin_spec_dir + '/fixtures/'
+RSpec.configure do |c|
+  c.fixture_path = plugin_spec_dir + '/fixtures/'
 end
